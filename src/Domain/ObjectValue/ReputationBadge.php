ad934e21068628a36cb1200c7d96749ac70b36ba
Rafael Caviquioli <rafaelcitj@gmail.com>
2019-11-10 18:07:01 -0300

    Refactor reputation badge

--- src/Domain/ObjectValue/ReputationBadge.php
+++ src/Domain/ObjectValue/ReputationBadge.php
@@ -10,40 +10,37 @@ class ReputationBadge
     public const YELLOW = "yellow";
     public const GREEN = "green";
 
+    private $reputation;
     private $badge;
 
     public function __construct(int $reputation)
     {
-        $this->badge = $this->reputationToBadge($reputation);
+        $this->reputation = $reputation;
+        
+        if ($this->isRed()) {
+            $this->badge = self::RED;
+        } elseif ($this->isYellow()) {
+            $this->badge = self::YELLOW;
+        } elseif ($this->isGreen()) {
+            $this->badge = self::GREEN;
+        } else {
+            throw new Exception("Was not possible define badge from reputation: '{$this->reputation}'");
+        }
     }
 
-    private function reputationToBadge($reputation): string
+    private function isRed(): bool
     {
-        if (self::isRed($reputation))
-            return self::RED;
-
-        if (self::isYellow($reputation))
-            return self::YELLOW;
-
-        if (self::isGreen($reputation))
-            return self::GREEN;
-
-        throw new Exception("Was not possible define badge from reputation: '$reputation'");
+        return $this->reputation <= 500;
     }
 
-    private static function isRed($reputation): bool
+    private function isYellow(): bool
     {
-        return $reputation <= 500;
+        return $this->reputation > 500 && $this->reputation <= 799;
     }
 
-    private static function isYellow($reputation): bool
+    private function isGreen(): bool
     {
-        return $reputation > 500 && $reputation <= 799;
-    }
-
-    private static function isGreen($reputation): bool
-    {
-        return $reputation > 799;
+        return $this->reputation > 799;
     }
 
     public function __toString(): string
